f74a35fe11c221c6e0ee1393146695ae39736499
rishabh-xx <118546372+rishabh-xx@users.noreply.github.com>
2022-11-21 20:33:21 +0530

    Update decorators.py

--- userbot/core/decorators.py
+++ userbot/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "Only My Master can Access This !!\n\nDeploy your own Catuserbot."
+                or "Only My Master RisHABH can Access This !!\n\nDeploy your own BoT... <ASK MY MASTER RISHABH MAY BE HE WILL HELP YOU TO DEVELOP>."
             )
             await c_q.answer(
                 HELP_TEXT,
